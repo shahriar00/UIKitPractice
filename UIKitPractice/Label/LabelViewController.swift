b81d181daa053993e6de21b0c5c6bf3ecd87361a
shahriar00 <shahriarsazid783@gmail.com>
2025-07-19 23:18:31 +0600

    storyboard basic and programatic auto layout creation

---
+++ UIKitPractice/Label/LabelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LabelViewController.swift
+//  UIKitPractice
+//
+//  Created by Shahriar Islam Sazid on 19/7/25.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
