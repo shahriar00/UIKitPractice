4e00829dbccd6e9519e65fc44a49051b51f687bf
shahriar00 <shahriarsazid783@gmail.com>
2025-07-19 12:13:58 +0600

    Initial Commit

---
+++ UIKitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPractice
+//
+//  Created by Shahriar Islam Sazid on 19/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UIKitPracticeTests/UIKitPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitPracticeTests.swift
+//  UIKitPracticeTests
+//
+//  Created by Shahriar Islam Sazid on 19/7/25.
+//
+
+import Testing
+@testable import UIKitPractice
+
+struct UIKitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
