3ddad57595b0cb9766f50f7602631e78f5bf5571
shahriar00 <shahriarsazid783@gmail.com>
2025-07-23 21:48:41 +0600

    button view controllers added

--- UIKitPractice/Button/ButtonViewController.swift
+++ UIKitPractice/Button/ButtonViewController.swift
@@ -8,22 +8,54 @@
 import UIKit
 
 class ButtonViewController: UIViewController {
+    
+    private var showNameButton : UIButton = {
+        let button = UIButton (type: .system)
+        button.setTitle( "Show Name", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.textAlignment = .center
+        button.setTitleColor(.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var subTitle : UILabel = {
+        let label = UILabel()
+        label.text = "Subtitle of the hello world"
+        label.textColor = .secondaryLabel
+        return label
+    }()
 
     override func viewDidLoad() {
+        
+        showNameButton.addTarget(self, action: #selector(Selectors), for: .touchUpInside)
         super.viewDidLoad()
+        
+        view.addSubview(showNameButton)
+        showNameButton.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        showNameButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        showNameButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        showNameButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        showNameButton.layer.cornerRadius = 25
+        showNameButton.backgroundColor = .systemBlue
+        
+        view.addSubview(subTitle)
+        subTitle.translatesAutoresizingMaskIntoConstraints = false
+        subTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        subTitle.topAnchor.constraint(equalTo: showNameButton.bottomAnchor, constant: 10).isActive = true
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+@objc func Selectors(){
+    subTitle.isHidden.toggle()
+    let title = subTitle.isHidden ? "Show Name" : "Hide Name"
+    showNameButton.setTitle(title, for: .normal)
     }
-    */
 
+}
+
+#Preview{
+    ButtonViewController()
 }
