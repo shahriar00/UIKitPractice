0b4dadfd1819e7fd2cefa4a0b4b5658e2e708772
shahriar00 <shahriarsazid783@gmail.com>
2025-07-29 22:41:43 +0600

    images and shapes view controller added

---
+++ UIKitPractice/Shapes/ShapesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShapesViewController.swift
+//  UIKitPractice
+//
+//  Created by Shahriar Islam Sazid on 7/29/25.
+//
+
+import UIKit
+
+class ShapesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
